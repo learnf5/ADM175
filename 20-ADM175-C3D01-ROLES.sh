07f7f15e1875d3da332293e5b90d84ebab780afb
learnf5 <99698691+learnf5@users.noreply.github.com>
2025-04-02 17:25:35 -0400

    Create 20-ADM175-C3D01-ROLES.sh

---
+++ 20-ADM175-C3D01-ROLES.sh
@@ -0,0 +1,8 @@
+# download config from GitHub and copy and load/merge it onto bigip1
+curl --silent https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/adm_part_user_role.scf --output /tmp/adm_part_user_role.scf
+sudo scp /tmp/adm_part_user_role.scf 192.168.1.31:/var/local/scf
+sudo ssh 192.168.1.31 tmsh load sys config merge file adm_part_user_role.scf
+
+# confirm bigip1 is active again
+sleep 5
+for i in {1..12}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
