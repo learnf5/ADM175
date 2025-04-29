5d9c80367f4275fde05c8bde1dfa26ade7cfbaf4
learnf5 <99698691+learnf5@users.noreply.github.com>
2025-04-29 14:38:08 -0400

    Update 20-ADM175-C2H01-PERSIST.sh

--- 20-ADM175-C2H01-PERSIST.sh
+++ 20-ADM175-C2H01-PERSIST.sh
@@ -3,6 +3,11 @@ curl --silent https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/src_addr
 sudo scp /tmp/src_addr_affin_persist.scf 192.168.1.31:/var/local/scf
 sudo ssh 192.168.1.31 tmsh load sys config merge file src_addr_affin_persist.scf
 
+# download config from GitHub and copy and load/merge to bigip1
+curl --silent https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/mng_obj_state.scf --output /tmp/mng_obj_state.scf
+sudo scp /tmp/mng_obj_state.scf 192.168.1.31:/var/local/scf
+###sudo ssh 192.168.1.31 tmsh load sys config merge file mng_obj_state.scf
+
 # confirm bigip1 is active 
 sleep 5
 for i in {1..12}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
