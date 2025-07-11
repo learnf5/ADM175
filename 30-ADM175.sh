d7c7e98bb61cfc86caff530762300ac5c8705492
djlamey <76072289+djlamey@users.noreply.github.com>
2025-07-11 12:13:04 -0400

    Update 30-ADM175.sh
    modified the curl command to use raw.githubusercontent.com

--- 30-ADM175.sh
+++ 30-ADM175.sh
@@ -1,5 +1,5 @@
 # copy archive files from GitHub to bigip1
-curl --silent https://github.com/learnf5/$COURSE_ID/raw/main/train_base_175.ucs --output /tmp/train_base.ucs
+curl --silent https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/train_base_175.ucs --output /tmp/train_base.ucs
 sudo scp /tmp/train_base.ucs 192.168.1.31:/var/local/ucs
 
 # confirm bigip1 is active
