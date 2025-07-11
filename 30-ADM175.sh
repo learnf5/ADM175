bb18f766ae12796a40161d9efe7071d8ea18fc64
djlamey <76072289+djlamey@users.noreply.github.com>
2025-07-11 11:11:26 -0400

    Create 30-ADM175.sh

---
+++ 30-ADM175.sh
@@ -0,0 +1,6 @@
+# copy archive files from GitHub to bigip1
+curl --silent https://github.com/learnf5/$COURSE_ID/raw/main/train_base_175.ucs --output /tmp/train_base.ucs
+sudo scp /tmp/train_base.ucs 192.168.1.31:/var/local/ucs
+
+# confirm bigip1 is active
+for i in {1..12}; do [ "$(sudo ssh root@192.168.1.31 cat /var/prompt/ps1)" = "Active" ] && break; sleep 5; done
