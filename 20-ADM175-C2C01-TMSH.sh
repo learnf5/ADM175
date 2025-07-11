ff1ed8dcb3efa769c70deab9fbf06e77cfecd41a
djlamey <76072289+djlamey@users.noreply.github.com>
2025-07-11 11:30:43 -0400

    Update 20-ADM175-C2C01-TMSH.sh
    removed file mgmt of train_base_X.ucs, moved to 30-ADM175.sh

--- 20-ADM175-C2C01-TMSH.sh
+++ 20-ADM175-C2C01-TMSH.sh
@@ -1,10 +1,7 @@
 # copy archive files from GitHub to bigip1
 curl --silent https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/cfg_w_tmsh.scf --output /tmp/cfg_w_tmsh.scf
 curl --silent https://raw.githubusercontent.com/learnf5/$COURSE_ID/main/train_base.scf --output /tmp/train_base.scf
-curl --silent https://github.com/learnf5/$COURSE_ID/raw/main/train_base_171.ucs --output /tmp/train_base_171.ucs
-mv /tmp/train_base_171.ucs /tmp/train_base.ucs
 sudo scp /tmp/*.scf 192.168.1.31:/var/local/scf
-sudo scp /tmp/train_base.ucs 192.168.1.31:/var/local/ucs
 
 ### WE MAY NEED TO SAVE THIS FOR THE SECOND OF TWO LABS (TMSH)
 # load/merge archive to bigip1 and pause
